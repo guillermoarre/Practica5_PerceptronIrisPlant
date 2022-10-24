3a089fb65e1e9ae8d6cd964ce019312fc9779e62
guillermoarre <guillermo.arredondo247@gmail.com>
2022-10-24 11:06:41 -0500

    Primer commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Practica5_PerceptronIris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.integrantesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arredondoVazquezGuillermoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rodríguezAguilarJesúsManuelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.lbx2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Setosa-Versicolor",
+            "Setosa-Virginica",
+            "Versicolor-Virginica"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 62);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Elige clases";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(899, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.integrantesToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // integrantesToolStripMenuItem
+            // 
+            this.integrantesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arredondoVazquezGuillermoToolStripMenuItem,
+            this.rodríguezAguilarJesúsManuelToolStripMenuItem});
+            this.integrantesToolStripMenuItem.Name = "integrantesToolStripMenuItem";
+            this.integrantesToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.integrantesToolStripMenuItem.Text = "Integrantes";
+            // 
+            // arredondoVazquezGuillermoToolStripMenuItem
+            // 
+            this.arredondoVazquezGuillermoToolStripMenuItem.Name = "arredondoVazquezGuillermoToolStripMenuItem";
+            this.arredondoVazquezGuillermoToolStripMenuItem.Size = new System.Drawing.Size(241, 22);
+            this.arredondoVazquezGuillermoToolStripMenuItem.Text = "Arredondo Vazquez Guillermo";
+            // 
+            // rodríguezAguilarJesúsManuelToolStripMenuItem
+            // 
+            this.rodríguezAguilarJesúsManuelToolStripMenuItem.Name = "rodríguezAguilarJesúsManuelToolStripMenuItem";
+            this.rodríguezAguilarJesúsManuelToolStripMenuItem.Size = new System.Drawing.Size(241, 22);
+            this.rodríguezAguilarJesúsManuelToolStripMenuItem.Text = "Rodríguez Aguilar Jesús Manuel";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dgv1
+            // 
+            this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgv1.Location = new System.Drawing.Point(139, 27);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(562, 394);
+            this.dgv1.TabIndex = 4;
+            // 
+            // lbx2
+            // 
+            this.lbx2.FormattingEnabled = true;
+            this.lbx2.Location = new System.Drawing.Point(708, 27);
+            this.lbx2.Name = "lbx2";
+            this.lbx2.Size = new System.Drawing.Size(170, 394);
+            this.lbx2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 33);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "PERCEPTRÓN\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbx2);
+            this.Controls.Add(this.dgv1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Practica 5, Perceptrón IRIS-PLANT";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem integrantesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arredondoVazquezGuillermoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rodríguezAguilarJesúsManuelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.ListBox lbx2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,330 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Practica5_PerceptronIris
+{
+    public partial class Form1 : Form
+    {
+        int indice, contador;
+        double w1, w2, w3, w4, umbral;
+        public Form1()
+        {
+            InitializeComponent();
+            generaTabla();
+            
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            indice = comboBox1.SelectedIndex;
+            if (indice == 0)
+            {
+                contador = 0;
+                ponerSetosaVersicolor();
+                borrarColumnaYcalc();
+            }
+            else if (indice == 1)
+            {
+                contador = 0;
+                ponerSetosaVirginica();
+                borrarColumnaYcalc();
+            }
+            else if (indice == 2)
+            {
+                contador = 0;
+                ponerVersicolorVirginica();
+                borrarColumnaYcalc();
+            }
+        }
+
+        public void ponerSetosaVersicolor()
+        {
+            int i;
+            string aux;
+            TextReader Leer = new StreamReader("datos.txt");
+            for (i = 0; i < 100; i++)
+            {         
+                aux = Leer.ReadLine();
+                dgv1.Rows[i].Cells[0].Value = aux.Substring(0,3);
+                dgv1.Rows[i].Cells[1].Value = aux.Substring(4,3);
+                dgv1.Rows[i].Cells[2].Value = aux.Substring(8,3);
+                dgv1.Rows[i].Cells[3].Value = aux.Substring(12,3);
+                if (i < 50)
+                {
+                    dgv1.Rows[i].Cells[4].Value = -1;
+                    dgv1.Rows[i].Cells[0].Style.BackColor = Color.LightSalmon;
+                    dgv1.Rows[i].Cells[1].Style.BackColor = Color.LightSalmon;
+                    dgv1.Rows[i].Cells[2].Style.BackColor = Color.LightSalmon;
+                    dgv1.Rows[i].Cells[3].Style.BackColor = Color.LightSalmon;
+                }
+                else 
+                {
+                    dgv1.Rows[i].Cells[4].Value = 1;
+                    dgv1.Rows[i].Cells[0].Style.BackColor = Color.LightPink;
+                    dgv1.Rows[i].Cells[1].Style.BackColor = Color.LightPink;
+                    dgv1.Rows[i].Cells[2].Style.BackColor = Color.LightPink;
+                    dgv1.Rows[i].Cells[3].Style.BackColor = Color.LightPink;
+                }
+                aux = "";
+            }
+            Leer.Close();
+        }
+        public void ponerSetosaVirginica()
+        {
+            int i;
+            string aux;
+            TextReader Leer = new StreamReader("datos.txt");
+            for (i = 0; i < 150; i++)
+            {
+                aux = Leer.ReadLine();
+                if (i < 50)
+                {
+                    dgv1.Rows[i].Cells[4].Value = -1;
+                    dgv1.Rows[i].Cells[0].Value = aux.Substring(0, 3);
+                    dgv1.Rows[i].Cells[1].Value = aux.Substring(4, 3);
+                    dgv1.Rows[i].Cells[2].Value = aux.Substring(8, 3);
+                    dgv1.Rows[i].Cells[3].Value = aux.Substring(12, 3);
+
+                    dgv1.Rows[i].Cells[0].Style.BackColor = Color.LightCyan;
+                    dgv1.Rows[i].Cells[1].Style.BackColor = Color.LightCyan;
+                    dgv1.Rows[i].Cells[2].Style.BackColor = Color.LightCyan;
+                    dgv1.Rows[i].Cells[3].Style.BackColor = Color.LightCyan;
+                }
+                else if (i > 99)
+                {
+                    dgv1.Rows[i-50].Cells[4].Value = 1;
+                    dgv1.Rows[i - 50].Cells[0].Value = aux.Substring(0, 3);
+                    dgv1.Rows[i - 50].Cells[1].Value = aux.Substring(4, 3);
+                    dgv1.Rows[i - 50].Cells[2].Value = aux.Substring(8, 3);
+                    dgv1.Rows[i - 50].Cells[3].Value = aux.Substring(12, 3);
+
+                    dgv1.Rows[i - 50].Cells[0].Style.BackColor = Color.LightGreen;
+                    dgv1.Rows[i - 50].Cells[1].Style.BackColor = Color.LightGreen;
+                    dgv1.Rows[i - 50].Cells[2].Style.BackColor = Color.LightGreen;
+                    dgv1.Rows[i - 50].Cells[3].Style.BackColor = Color.LightGreen;
+                }
+
+                aux = "";
+            }
+            Leer.Close();
+        }
+
+        public void ponerVersicolorVirginica()
+        {
+            int i;
+            string aux;
+            TextReader Leer = new StreamReader("datos.txt");
+            for (i = 0; i < 150; i++)
+            {
+                aux = Leer.ReadLine();
+                if (i > 49 && i <100)
+                {
+                    dgv1.Rows[i-50].Cells[4].Value = -1;
+                    dgv1.Rows[i - 50].Cells[0].Value = aux.Substring(0, 3);
+                    dgv1.Rows[i - 50].Cells[1].Value = aux.Substring(4, 3);
+                    dgv1.Rows[i - 50].Cells[2].Value = aux.Substring(8, 3);
+                    dgv1.Rows[i - 50].Cells[3].Value = aux.Substring(12, 3);
+
+                    dgv1.Rows[i - 50].Cells[0].Style.BackColor = Color.NavajoWhite;
+                    dgv1.Rows[i - 50].Cells[1].Style.BackColor = Color.NavajoWhite;
+                    dgv1.Rows[i - 50].Cells[2].Style.BackColor = Color.NavajoWhite;
+                    dgv1.Rows[i - 50].Cells[3].Style.BackColor = Color.NavajoWhite;
+                }
+                else if (i > 99)
+                {
+                    dgv1.Rows[i - 50].Cells[4].Value = 1;
+                    dgv1.Rows[i - 50].Cells[0].Value = aux.Substring(0, 3);
+                    dgv1.Rows[i - 50].Cells[1].Value = aux.Substring(4, 3);
+                    dgv1.Rows[i - 50].Cells[2].Value = aux.Substring(8, 3);
+                    dgv1.Rows[i - 50].Cells[3].Value = aux.Substring(12, 3);
+
+                    dgv1.Rows[i - 50].Cells[0].Style.BackColor = Color.Lavender;
+                    dgv1.Rows[i - 50].Cells[1].Style.BackColor = Color.Lavender;
+                    dgv1.Rows[i - 50].Cells[2].Style.BackColor = Color.Lavender;
+                    dgv1.Rows[i - 50].Cells[3].Style.BackColor = Color.Lavender;
+                }
+                aux = "";
+            }
+            Leer.Close();
+
+
+        }
+        public void generaTabla()
+        {
+            int m, n;
+            m = 100;
+            n = 6;
+            dgv1.RowCount = m;
+            dgv1.ColumnCount = n;
+            dgv1.Columns[0].HeaderText = "X1";
+            dgv1.Columns[1].HeaderText = "X2";
+            dgv1.Columns[2].HeaderText = "X3";
+            dgv1.Columns[3].HeaderText = "X4";
+            dgv1.Columns[4].HeaderText = "Yesp";
+            dgv1.Columns[4].DefaultCellStyle.BackColor = Color.LightYellow;
+            dgv1.Columns[5].HeaderText = "Ycal";
+            dgv1.Columns[5].DefaultCellStyle.BackColor = Color.LightBlue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //valores aleatorios
+            w1 = 1;
+            w2 = 1;
+            w3 = 1;
+            w4 = 1;
+            umbral = 0.5;
+            int i;
+            
+            lbx2.Items.Clear();
+            primerosValores();
+            do
+            {
+                for (i = 0; i < 100; i++)
+                {
+                    if (OperaAprendizaje5(dgv1.Rows[i].Cells[0].Value.ToString(), dgv1.Rows[i].Cells[1].Value.ToString(), dgv1.Rows[i].Cells[2].Value.ToString(), dgv1.Rows[i].Cells[3].Value.ToString()) == dgv1.Rows[i].Cells[4].Value.ToString())
+                    {
+                        //Clasificó bien          
+                    }
+                    else
+                    {
+                        //Clasificó mal
+                        OperaAprendizaje6(i);
+                    }
+                }
+                //Época terminada
+                //Evalua con los valores obtenidos
+            }
+            while (!EvaluaEpocaActual() && !(contador > 50));
+
+            if (contador > 50)
+            {
+                lbx2.Items.Add("");
+                lbx2.Items.Add("");
+                lbx2.Items.Add("->EPOCAS= "+contador.ToString());
+                lbx2.Items.Add("->Pesos");
+                lbx2.Items.Add("W1 = " + w1.ToString());
+                lbx2.Items.Add("W2 = " + w2.ToString());
+                lbx2.Items.Add("W3 = " + w3.ToString());
+                lbx2.Items.Add("W4 = " + w4.ToString());
+                lbx2.Items.Add("Umbral = " + umbral.ToString());
+                MessageBox.Show("Solución NO encontrada!!!!");
+            }
+            else
+            {
+                lbx2.Items.Add("");
+                lbx2.Items.Add("");
+                lbx2.Items.Add("->EPOCAS= " + contador.ToString());
+                lbx2.Items.Add("->Pesos");
+                lbx2.Items.Add("W1 = " + w1.ToString());
+                lbx2.Items.Add("W2 = " + w2.ToString());
+                lbx2.Items.Add("W3 = " + w3.ToString());
+                lbx2.Items.Add("W4 = " + w4.ToString());
+                lbx2.Items.Add("Umbral = " + umbral.ToString());
+                MessageBox.Show("Solución encontrada!!!!");
+            }
+
+        }
+        public string OperaAprendizaje5(string x1, string x2, string x3, string x4)
+        {
+            double x1d, x2d, x3d, x4d, var;
+            x1d = double.Parse(x1);
+            x2d = double.Parse(x2);
+            x3d = double.Parse(x3);
+            x4d = double.Parse(x4);
+            var = x1d * w1 + x2d * w2 + x3d * w3 + x4d * w4 + umbral;
+            if (var > 0)
+            {
+                return "1";
+            }
+            else
+            {
+                return "-1";
+            }
+
+        }
+        public void OperaAprendizaje6(int fila)
+        {
+            double dx, dw1, dw2, dw3, dw4, dw0, x1, x2, x3, x4;
+            dx = dw1 = dw2 = dw3 = dw4 = dw0 = x1 = x2 = x3 = x4 = 0.0;
+            x1 = double.Parse(dgv1.Rows[fila].Cells[0].Value.ToString());
+            x2 = double.Parse(dgv1.Rows[fila].Cells[1].Value.ToString());
+            x3 = double.Parse(dgv1.Rows[fila].Cells[2].Value.ToString());
+            x4 = double.Parse(dgv1.Rows[fila].Cells[3].Value.ToString());
+            dx = double.Parse(dgv1.Rows[fila].Cells[4].Value.ToString());
+            dw1 = dx * x1;
+            dw2 = dx * x2;
+            dw3 = dx * x3;
+            dw4 = dx * x4;
+            w1 = w1 + dw1;
+            w2 = w2 + dw2;
+            w3 = w3 + dw3;
+            w4 = w4 + dw4;
+            umbral = umbral + dx;
+
+        }
+        public bool EvaluaEpocaActual()
+        {
+            bool flagaux = true;
+            contador++;
+            int i;
+            lbx2.Items.Add("");
+            lbx2.Items.Add("\nEpoca = " + contador.ToString());
+            for (i = 0; i < 100; i++)
+            {
+                if (OperaAprendizaje5(dgv1.Rows[i].Cells[0].Value.ToString(), dgv1.Rows[i].Cells[1].Value.ToString(), dgv1.Rows[i].Cells[2].Value.ToString(), dgv1.Rows[i].Cells[3].Value.ToString()) == dgv1.Rows[i].Cells[4].Value.ToString())
+                {
+                    //Se Clasificó bien
+                    dgv1.Rows[i].Cells[5].Value = OperaAprendizaje5(dgv1.Rows[i].Cells[0].Value.ToString(), dgv1.Rows[i].Cells[1].Value.ToString(), dgv1.Rows[i].Cells[2].Value.ToString(), dgv1.Rows[i].Cells[3].Value.ToString());
+                }
+                else
+                {
+                    //Se Clasificó mal
+                    dgv1.Rows[i].Cells[5].Value = OperaAprendizaje5(dgv1.Rows[i].Cells[0].Value.ToString(), dgv1.Rows[i].Cells[1].Value.ToString(), dgv1.Rows[i].Cells[2].Value.ToString(), dgv1.Rows[i].Cells[3].Value.ToString());
+                    flagaux = false;
+                }
+                lbx2.Items.Add("->Pesos");
+                lbx2.Items.Add("W1 = " + w1.ToString());
+                lbx2.Items.Add("W2 = " + w2.ToString());
+                lbx2.Items.Add("W3 = " + w3.ToString());
+                lbx2.Items.Add("W4 = " + w4.ToString());
+                lbx2.Items.Add("Umbral = " + umbral.ToString());
+                lbx2.Items.Add("->Entrada " + i.ToString() + "\n");
+            }
+            return flagaux;
+
+        }
+        public void primerosValores()
+        {
+                lbx2.Items.Add("->Pesos Origs.");
+                lbx2.Items.Add("W1 = " + w1.ToString());
+                lbx2.Items.Add("W2 = " + w2.ToString());
+                lbx2.Items.Add("W3 = " + w3.ToString());
+                lbx2.Items.Add("W4 = " + w4.ToString());
+                lbx2.Items.Add("Umbral Orig. = " + umbral.ToString());
+        }
+        public void borrarColumnaYcalc()
+        {
+            int i;
+            for (i = 0; i < 100; i++)
+            {
+                dgv1.Rows[i].Cells[5].Value = "";
+            }
+        
+        }
+
+    }
+}
